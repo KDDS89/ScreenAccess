7a20e64577e0ec6e07166aa014c51f301c5f3e2b
Keith Stuart <keith.stuart@newgrove.com>
2020-08-24 15:09:31 +0100

    Prowler

--- ScreenAssistant/Games/ApLeg/Weapons/Prowler.cs
+++ ScreenAssistant/Games/ApLeg/Weapons/Prowler.cs
@@ -9,11 +9,49 @@
 
         public override double AdjustMouse(int shotNumber)
         {
-            this.AdjustmentCoefficient = CalculateAdjustment(shotNumber, 5);
-            var horizontalOffset = 0;
-            var verticalOffset = Rnd.NextDouble() * 1 + 5.5d;
+            double horizontalOffset;
+            double verticalOffset;
+            this.AdjustmentCoefficient = CalculateAdjustment(shotNumber, 35);
+
+            if (shotNumber < 1)
+            {
+                verticalOffset = Rnd.NextDouble() + 9d;
+                horizontalOffset = 0;
+            }
+            else if (shotNumber < 6)
+            {
+                verticalOffset = Rnd.NextDouble()  + 7d;
+                horizontalOffset = Rnd.NextDouble() + 0.5d;
+            }
+            else if (shotNumber < 13)
+            {
+                verticalOffset = Rnd.NextDouble() + 3.5d;
+                horizontalOffset = Rnd.NextDouble() + 2d;
+            }
+            else if (shotNumber < 18)
+            {
+                verticalOffset = Rnd.NextDouble() + 4d;
+                horizontalOffset = Rnd.NextDouble() - 6d;
+            }
+            else if (shotNumber < 29)
+            {
+                verticalOffset = Rnd.NextDouble() + 5d;
+                horizontalOffset = 0;
+            }
+            else if (shotNumber < 33)
+            {
+                verticalOffset = Rnd.NextDouble();
+                horizontalOffset = Rnd.NextDouble() - 9d;
+            }
+            else
+            {
+                verticalOffset = Rnd.NextDouble() + 6d;
+                horizontalOffset = Rnd.NextDouble() + 1d;
+            }
+
             this.MoveMouse(horizontalOffset, verticalOffset);
-            return this.GetAdjustmentTime(0.3d);
+
+            return this.GetAdjustmentTime(1d);
         }
     }
 }
